0c4e60c5dbd12b0795f3be7f7b6d779a33d1a33e
jallmenroeder <jallmenroeder@gmail.com>
2021-02-18 20:42:08 +0100

    add sanity test for BRDF sampling

---
+++ Test.h
@@ -0,0 +1,14 @@
+//
+// Created by jallmenroeder on 15.02.21.
+//
+
+#pragma once
+
+#include <memory>
+
+#include "SphericalFunctions/BRDF.h"
+
+class Test {
+public:
+	static bool testBrdfSampling(BRDF* brdf);
+};
